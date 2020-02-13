4faf753cf17e5405abb2859df3bc8389d397cde4
Igor Srdoč <igor@srdoc.si>
2020-02-13 17:55:32 +0100

    Add User model. Add psycopg2 for Heroku database. Add user info to index.html

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from models import User, db
 
 app = Flask(__name__)
+db.create_all()
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    email_address = request.cookies.get("email")
 
+    # get user from the database based on email address
+    user = db.query(User).filter_by(email=email_address).first()
+
+    return render_template("index.html", user=user)
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form.get("user-name")
+    email = request.form.get("user-email")
+
+    # create a User object
+    user = User(name=name, email=email)
+
+    # save the user object into a database
+    db.add(user)
+    db.commit()
+
+    # save user's email into a cookie
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie("email", email)
+
+    return response
 
 if __name__ == '__main__':
     app.run()
